0a4143a985fa4ea6aae4dc5a583cf91e8c885b7e
Keita Nakata <0024481294bxyk6037@gmail.com>
2023-03-24 09:57:20 +0900

    add: js animation

--- js/script.js
+++ js/script.js
@@ -18,3 +18,13 @@ $(function () {
   });
 
 });
+
+
+bubbly();
+
+var particles = Particles.init({
+  selector: '.background',
+  sizeVariations: 10,
+  color: ['#00bbdd', '#404B69', '#DBEDF3'],
+  connectParticles: true
+});
