c06d25defb17c05ec0bfff9940198c1050b1616d
Keita Nakata <nakata.keita.12@gmail.com>
2023-08-01 11:23:34 +0900

    Update redirect1.php

--- lab/redirect1.php
+++ lab/redirect1.php
@@ -3,7 +3,7 @@
 <?php
 
 $CLIENTID="1rusOLs0QDeo9GeTYiMXCQ";
-$REDIRECTURI="https://tyukei.github.io/portfolio.github.io/lab/redirect.php";
+$REDIRECTURI="https://tyukei.github.io/portfolio.github.io/lab/redirect2.php";
 $REQUESTURI="https://zoom.us/oauth/authorize?response_type=code&client_id=" . $CLIENTID . "&redirect_uri=" . $REDIRECTURI;
 
 echo("<a href=" . $REQUESTURI . "> Go! </a>")
