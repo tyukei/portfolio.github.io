67f83084894bca05992ab484b177806a84cf7d45
Keita Nakata <nakata.keita.12@gmail.com>
2023-08-01 11:11:58 +0900

    Update and rename redirect.html to redirect.php

--- lab/redirect.php
+++ lab/redirect.php
@@ -3,7 +3,7 @@
 <?php
 
 $CLIENTID="i8NJn5vETrCUX5TUCru3Pw";
-$REDIRECTURI="http://www..../rcvauth.php";
+$REDIRECTURI="https://tyukei.github.io/portfolio.github.io/lab/redirect.php";
 $REQUESTURI="https://zoom.us/oauth/authorize?response_type=code&client_id=" . $CLIENTID . "&redirect_uri=" . $REDIRECTURI;
 
 echo("<a href=" . $REQUESTURI . "> Go! </a>")
